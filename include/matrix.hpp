0ce12aa946a3bf7e5b0f381e8069b53131081c2c
Maxim Alexandrov <maxim_0101@bk.ru>
2024-12-30 02:46:21 +0300

    Немного эстетически изменил код

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <cstdint>
 #include <initializer_list>
 #include <stdexcept>
 #include <vector>
@@ -17,7 +16,7 @@ private:
         using reverse_iterator = typename std::vector<T>::reverse_iterator;
         using const_reverse_iterator = typename std::vector<T>::const_reverse_iterator;
 
-        constexpr ProxyRow(size_t cols, const T& value = T()) : proxy_data_(cols, value) {}
+        constexpr ProxyRow(size_t cols, const T& value = T()) : ProxyData_(cols, value) {}
         constexpr ProxyRow(const ProxyRow&) = default;
         constexpr ProxyRow(ProxyRow&&) = default;
         constexpr ProxyRow& operator=(const ProxyRow&) = default;
@@ -25,57 +24,57 @@ private:
         constexpr bool operator==(const ProxyRow& other) const;
         
 
-        constexpr ProxyRow(const std::vector<T>& other) : proxy_data_(other) {}
-        constexpr ProxyRow(std::vector<T>&& other) : proxy_data_(std::move(other)) {}
+        constexpr ProxyRow(const std::vector<T>& other) : ProxyData_(other) {}
+        constexpr ProxyRow(std::vector<T>&& other) : ProxyData_(std::move(other)) {}
         constexpr ProxyRow& operator=(const std::vector<T>& other);
         constexpr ProxyRow& operator=(std::vector<T>&& other);
         constexpr bool operator==(const std::vector<T>& other) const;
         
-        ProxyRow(std::initializer_list<T> list) : proxy_data_(list) {}
+        ProxyRow(std::initializer_list<T> list) : ProxyData_(list) {}
         
         operator std::vector<T>() const; 
 
         friend bool operator==(const std::vector<T>& vec, const ProxyRow& row) {
-            return vec == row.proxy_data_;
+            return vec == row.ProxyData_;
         }
 
         constexpr size_t size() const noexcept;
 
-        constexpr iterator begin() noexcept { return proxy_data_.begin(); }
-        constexpr const_iterator begin() const noexcept { return proxy_data_.begin(); }
-        constexpr iterator end() noexcept { return proxy_data_.end(); }
-        constexpr const_iterator end() const noexcept { return proxy_data_.end(); }
+        constexpr iterator begin() noexcept { return ProxyData_.begin(); }
+        constexpr const_iterator begin() const noexcept { return ProxyData_.begin(); }
+        constexpr iterator end() noexcept { return ProxyData_.end(); }
+        constexpr const_iterator end() const noexcept { return ProxyData_.end(); }
 
-        constexpr const_iterator cbegin() const noexcept { return proxy_data_.cbegin(); }
-        constexpr const_iterator cend() const noexcept { return proxy_data_.cend(); }
+        constexpr const_iterator cbegin() const noexcept { return ProxyData_.cbegin(); }
+        constexpr const_iterator cend() const noexcept { return ProxyData_.cend(); }
 
-        constexpr reverse_iterator rbegin() noexcept { return proxy_data_.rbegin(); }
-        constexpr const_reverse_iterator rbegin() const noexcept { return proxy_data_.rbegin(); }
-        constexpr reverse_iterator rend() noexcept { return proxy_data_.rend(); }
-        constexpr const_reverse_iterator rend() const noexcept { return proxy_data_.rend(); }
+        constexpr reverse_iterator rbegin() noexcept { return ProxyData_.rbegin(); }
+        constexpr const_reverse_iterator rbegin() const noexcept { return ProxyData_.rbegin(); }
+        constexpr reverse_iterator rend() noexcept { return ProxyData_.rend(); }
+        constexpr const_reverse_iterator rend() const noexcept { return ProxyData_.rend(); }
 
-        constexpr const_reverse_iterator crbegin() const noexcept { return proxy_data_.crbegin(); }
-        constexpr const_reverse_iterator crend() const noexcept { return proxy_data_.crend(); }
+        constexpr const_reverse_iterator crbegin() const noexcept { return ProxyData_.crbegin(); }
+        constexpr const_reverse_iterator crend() const noexcept { return ProxyData_.crend(); }
 
         T& operator[](size_t col);
         const T& operator[](size_t col) const;
 
-        std::vector<T> proxy_data_;
+        std::vector<T> ProxyData_;
     };
 public:
     using value_type = T;
     TMatrix() = default;
-    constexpr TMatrix(size_t rows, size_t cols, const T& value = T()) : data_(rows, ProxyRow(cols, value)) {}
+    constexpr TMatrix(size_t rows, size_t cols, const T& value = T()) : Data_(rows, ProxyRow(cols, value)) {}
     constexpr TMatrix(const TMatrix&) = default;
     constexpr TMatrix(TMatrix&&) = default;
     constexpr TMatrix& operator=(const TMatrix&) = default;
     constexpr TMatrix& operator=(TMatrix&&) = default;
 
-    constexpr TMatrix(const std::vector<ProxyRow>& data) : data_(data) {}
+    constexpr TMatrix(const std::vector<ProxyRow>& data) : Data_(data) {}
     TMatrix(std::initializer_list<std::initializer_list<T>> list);
     
-    const size_t rows() const noexcept;
-    const size_t cols() const noexcept;
+    const size_t Rows() const noexcept;
+    const size_t Cols() const noexcept;
 
 	ProxyRow& operator[](size_t row);
 	const ProxyRow& operator[](size_t row) const;
@@ -99,7 +98,9 @@ public:
     TMatrix operator*(const T&) const;
     TMatrix operator/(const T&) const;
 	
-    TMatrix transpose() const;
+    // TODO: operator std::vector<std::vector<T>>() const;
+
+    TMatrix Transpose() const;
 
     bool operator==(const TMatrix&) const;
     // TODO: add concepts
@@ -108,54 +109,54 @@ public:
 
 private:
     // TODO: use pimpl
-    std::vector<ProxyRow> data_;
+    std::vector<ProxyRow> Data_;
 };
 
 template <typename T>
 TMatrix<T>::ProxyRow::operator std::vector<T>() const {
-    return proxy_data_;
+    return ProxyData_;
 } 
 
 template <typename T>
 constexpr typename TMatrix<T>::ProxyRow& TMatrix<T>::ProxyRow::operator=(const std::vector<T>& other) {
-    proxy_data_ = other;
+    ProxyData_ = other;
     return *this;
 }
 
 template <typename T>
 constexpr typename TMatrix<T>::ProxyRow& TMatrix<T>::ProxyRow::operator=(std::vector<T>&& other) {
-    proxy_data_ = std::move(other);
+    ProxyData_ = std::move(other);
     return *this;
 }
 
 template <typename T>
 constexpr bool TMatrix<T>::ProxyRow::operator==(const ProxyRow& other) const {
-    return proxy_data_ == other.proxy_data_;
+    return ProxyData_ == other.ProxyData_;
 }
 
 template <typename T>
 constexpr bool TMatrix<T>::ProxyRow::operator==(const std::vector<T>& other) const {
-    return proxy_data_ == other;
+    return ProxyData_ == other;
 }
 
 template <typename T>
 constexpr size_t TMatrix<T>::ProxyRow::size() const noexcept {
-    return proxy_data_.size();
+    return ProxyData_.size();
 } 
 
 template <typename T>
 T& TMatrix<T>::ProxyRow::operator[](size_t col) {
-    if (col >= proxy_data_.size()) {
+    if (col >= ProxyData_.size()) {
         throw std::out_of_range("Index out of range");
     }
-    return proxy_data_[col];
+    return ProxyData_[col];
 }
 template <typename T>
 const T& TMatrix<T>::ProxyRow::operator[](size_t col) const {
-    if (col >= proxy_data_.size()) {
+    if (col >= ProxyData_.size()) {
         throw std::out_of_range("Index out of range");
     }
-    return proxy_data_[col];
+    return ProxyData_[col];
 }
 
 
@@ -164,46 +165,46 @@ TMatrix<T>::TMatrix(std::initializer_list<std::initializer_list<T>> list) {
     size_t rows_ = list.size();
     size_t cols_ = list.begin()->size();
 
-    data_.reserve(rows_);
+    Data_.reserve(rows_);
     for (const auto& row : list) {
         if (row.size() != cols_) {
             throw std::invalid_argument("All rows must have the same number of columns");
         }
-        data_.emplace_back(row);
+        Data_.emplace_back(row);
     }
 }
 
 template <typename T>
-const size_t TMatrix<T>::rows() const noexcept { return data_.size(); }
+const size_t TMatrix<T>::Rows() const noexcept { return Data_.size(); }
 
 
 template <typename T>
-const size_t TMatrix<T>::cols() const noexcept { return data_.empty() ? 0 : data_.front().size(); }
+const size_t TMatrix<T>::Cols() const noexcept { return Data_.empty() ? 0 : Data_.front().size(); }
 
 
 template <typename T>
 typename TMatrix<T>::ProxyRow& TMatrix<T>::operator[](size_t row) {
-    if (row >= rows()) {
+    if (row >= Rows()) {
         throw std::out_of_range("Index out of range");
     }
-    return data_[row];
+    return Data_[row];
 }
 
 template <typename T>
 const typename TMatrix<T>::ProxyRow& TMatrix<T>::operator[](size_t row) const {
-    if (row >= rows()) {
+    if (row >= Rows()) {
         throw std::out_of_range("Index out of range");
     }
-    return data_[row];
+    return Data_[row];
 }
 
 template <typename T>
 TMatrix<T>& TMatrix<T>::operator+=(const TMatrix<T>& other) {
-    if (rows() != other.rows() || cols() != other.cols()) {
+    if (Rows() != other.Rows() || Cols() != other.Cols()) {
         throw std::invalid_argument("Matrix dimensions must match for addition");
     }
-    for (size_t i = 0; i < rows(); ++i) {
-        for (size_t j = 0; j < cols(); ++j) {
+    for (size_t i = 0; i < Rows(); ++i) {
+        for (size_t j = 0; j < Cols(); ++j) {
             (*this)[i][j] += other[i][j];
         }
     }
@@ -212,11 +213,11 @@ TMatrix<T>& TMatrix<T>::operator+=(const TMatrix<T>& other) {
 
 template <typename T>
 TMatrix<T>& TMatrix<T>::operator-=(const TMatrix<T>& other) {
-    if (rows() != other.rows() || cols() != other.cols()) {
+    if (Rows() != other.Rows() || Cols() != other.Cols()) {
         throw std::invalid_argument("Matrix dimensions must match for subtraction");
     }
-    for (size_t i = 0; i < rows(); ++i) {
-        for (size_t j = 0; j < cols(); ++j) {
+    for (size_t i = 0; i < Rows(); ++i) {
+        for (size_t j = 0; j < Cols(); ++j) {
             (*this)[i][j] -= other[i][j];
         }
     }
@@ -225,15 +226,15 @@ TMatrix<T>& TMatrix<T>::operator-=(const TMatrix<T>& other) {
 
 template <typename T>
 TMatrix<T>& TMatrix<T>::operator*=(const TMatrix<T>& other) {
-    if (cols() != other.rows()) {
+    if (Cols() != other.Rows()) {
         throw std::invalid_argument("Matrix dimensions are not suitable for multiplication");
     }
 
-    TMatrix<T> result(rows(), other.cols(), T{});
+    TMatrix<T> result(Rows(), other.Cols(), T{});
 
-    for (size_t i = 0; i < rows(); ++i) {
-        for (size_t j = 0; j < other.cols(); ++j) {
-            for (size_t k = 0; k < cols(); ++k) {
+    for (size_t i = 0; i < Rows(); ++i) {
+        for (size_t j = 0; j < other.Cols(); ++j) {
+            for (size_t k = 0; k < Cols(); ++k) {
                 result[i][j] += (*this)[i][k] * other[k][j];
             }
         }
@@ -265,7 +266,7 @@ TMatrix<T> TMatrix<T>::operator*(const TMatrix<T>& other) const {
 
 template <typename T>
 TMatrix<T>& TMatrix<T>::operator+=(const T& scalar) {
-    for (auto& rows : data_) {
+    for (auto& rows : Data_) {
         for (auto& element : rows) {
             element += scalar;
         }
@@ -275,7 +276,7 @@ TMatrix<T>& TMatrix<T>::operator+=(const T& scalar) {
 
 template <typename T>
 TMatrix<T>& TMatrix<T>::operator-=(const T& scalar) {
-    for (auto& rows : data_) {
+    for (auto& rows : Data_) {
         for (auto& element : rows) {
             element -= scalar;
         }
@@ -285,7 +286,7 @@ TMatrix<T>& TMatrix<T>::operator-=(const T& scalar) {
 
 template <typename T>
 TMatrix<T>& TMatrix<T>::operator*=(const T& scalar) {
-    for (auto& rows : data_) {
+    for (auto& rows : Data_) {
         for (auto& element : rows) {
             element *= scalar;
         }
@@ -299,7 +300,7 @@ TMatrix<T>& TMatrix<T>::operator/=(const T& scalar) {
     if (scalar == zero) {
         throw std::runtime_error("Division by zero!");
     }
-    for (auto& rows : data_) {
+    for (auto& rows : Data_) {
         for (auto& element : rows) {
             element /= scalar;
         }
@@ -332,10 +333,10 @@ TMatrix<T> TMatrix<T>::operator/(const T& scalar) const {
 }
 
 template <typename T>
-TMatrix<T> TMatrix<T>::transpose() const {
-	TMatrix<T> result(cols(), rows());
-	for (size_t i = 0; i < rows(); ++i) {
-		for (size_t j = 0; j < cols(); ++j) {
+TMatrix<T> TMatrix<T>::Transpose() const {
+	TMatrix<T> result(Cols(), Rows());
+	for (size_t i = 0; i < Rows(); ++i) {
+		for (size_t j = 0; j < Cols(); ++j) {
 			result[j][i] = (*this)[i][j];
 		}
 	}
@@ -344,12 +345,12 @@ TMatrix<T> TMatrix<T>::transpose() const {
 
 template <typename T>
 bool TMatrix<T>::operator==(const TMatrix<T>& other) const {
-	return data_ == other.data_;
+	return Data_ == other.Data_;
 }
 
 template <typename T>
 std::ostream& operator<<(std::ostream& out, const TMatrix<T>& matrix) {
-	for (const auto& row : matrix.data_) {
+	for (const auto& row : matrix.Data_) {
 		for (const auto& elem : row) {
 			out << elem << ' ';
 		}
--- tests/matrix_test.cpp
+++ tests/matrix_test.cpp
@@ -5,84 +5,84 @@
 using namespace NMatrix;
 
 TEST(MatrixConstructorTest, DefaultConstructor) {
-    NMatrix::TMatrix<> mat;
-    EXPECT_EQ(mat.rows(), 0.0);
-    EXPECT_EQ(mat.cols(), 0.0);
+    NMatrix::TMatrix<> matrix;
+    EXPECT_EQ(matrix.Rows(), 0.0);
+    EXPECT_EQ(matrix.Cols(), 0.0);
 }
 
 TEST(MatrixConstructorTest, ParameterizedConstructor) {
-    TMatrix<> mat(1, 3);
+    TMatrix<> matrix(1, 3);
 
     TMatrix<> expected{{0, 0, 0}};
-    EXPECT_EQ(mat, expected);
+    EXPECT_EQ(matrix, expected);
 }
 
 TEST(MatrixConstructorTest, ParameterizedConstructorWithDefaultValue) {
-    TMatrix<> mat(3, 3, 1.0);
-    EXPECT_EQ(mat.rows(), 3);
-    EXPECT_EQ(mat.cols(), 3);
+    TMatrix<> matrix(3, 3, 1.0);
+    EXPECT_EQ(matrix.Rows(), 3);
+    EXPECT_EQ(matrix.Cols(), 3);
 
     TMatrix<> expected{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
-    EXPECT_EQ(mat, expected);
+    EXPECT_EQ(matrix, expected);
 }
 
 TEST(MatrixAccessTest, OutOfBoundsAccessRows) {
-    TMatrix<> mat(3, 3, 1.0);
-    EXPECT_THROW(mat[3][3], std::out_of_range);
+    TMatrix<> matrix(3, 3, 1.0);
+    EXPECT_THROW(matrix[3][3], std::out_of_range);
 }
 
 TEST(MatrixAccessTest, OutOfBoundsAccessCols) {
-    TMatrix<> mat(3, 3, 1.0);
-    EXPECT_THROW(mat[0][3], std::out_of_range);
+    TMatrix<> matrix(3, 3, 1.0);
+    EXPECT_THROW(matrix[0][3], std::out_of_range);
 }
 
 TEST(MatrixAccessTest, Rows) {
-    TMatrix<> mat(1, 1);
-    EXPECT_EQ(mat.rows(), 1);
+    TMatrix<> matrix(1, 1);
+    EXPECT_EQ(matrix.Rows(), 1);
 }
 
 TEST(MatrixAccessTest, Cols) {
-    TMatrix<> mat(1, 1);
-    EXPECT_EQ(mat.cols(), 1);
+    TMatrix<> matrix(1, 1);
+    EXPECT_EQ(matrix.Cols(), 1);
 }
 
 TEST(MatrixOperationsTest, AdditionCorrect) {
-    TMatrix<> mat1(2, 2, 1.0);
-    TMatrix<> mat2(2, 2, 2.0);
-    TMatrix<> result = mat1 + mat2;
+    TMatrix<> matrix1(2, 2, 1.0);
+    TMatrix<> matrix2(2, 2, 2.0);
+    TMatrix<> result = matrix1 + matrix2;
 
     TMatrix<> expected{{3, 3}, {3, 3}};
     EXPECT_EQ(result, expected);
 }
 
 TEST(MatrixOperationsTest, AdditionIncorrect) {
-    TMatrix<> mat1(1, 1);
-    TMatrix<> mat2(1, 2);
+    TMatrix<> matrix1(1, 1);
+    TMatrix<> matrix2(1, 2);
 
-    EXPECT_THROW(mat1 + mat2, std::invalid_argument);
+    EXPECT_THROW(matrix1 + matrix2, std::invalid_argument);
 }
 
 
 TEST(MatrixOperationsTest, SubtractionCorrect) {
-    TMatrix<> mat1(2, 2, 3.0);
-    TMatrix<> mat2(2, 2, 2.0);
-    TMatrix<> result = mat1 - mat2;
+    TMatrix<> matrix1(2, 2, 3.0);
+    TMatrix<> matrix2(2, 2, 2.0);
+    TMatrix<> result = matrix1 - matrix2;
 
     TMatrix<> expected{{1, 1}, {1, 1}};
     EXPECT_EQ(result, expected);
 }
 
 TEST(MatrixOperationsTest, SubtractionInorrect) {
-    TMatrix<> mat1(1, 1, 3.0);
-    TMatrix<> mat2(2, 2, 2.0);
+    TMatrix<> matrix1(1, 1, 3.0);
+    TMatrix<> matrix2(2, 2, 2.0);
 
-    EXPECT_THROW(mat1 - mat2, std::invalid_argument);
+    EXPECT_THROW(matrix1 - matrix2, std::invalid_argument);
 }
 
 TEST(MatrixOperationsTest, MultiplicationCorrect) {
-    TMatrix<> mat1{{2, -3, 1}, {5, 4, -2}};
-    TMatrix<> mat2{{-7, 5}, {2, -1}, {4, 3}};
-    TMatrix<> result = mat1 * mat2;
+    TMatrix<> matrix1{{2, -3, 1}, {5, 4, -2}};
+    TMatrix<> matrix2{{-7, 5}, {2, -1}, {4, 3}};
+    TMatrix<> result = matrix1 * matrix2;
 
     TMatrix<> expected{{-16, 16}, {-35, 15}};
     EXPECT_EQ(result, expected);
@@ -90,107 +90,107 @@ TEST(MatrixOperationsTest, MultiplicationCorrect) {
 
 
 TEST(MatrixOperationsTest, MultiplicationIncorrect) {
-    TMatrix<> mat1{{1}, {2}};
-    TMatrix<> mat2{{2, 0}, {1, 2}};
+    TMatrix<> matrix1{{1}, {2}};
+    TMatrix<> matrix2{{2, 0}, {1, 2}};
 
-    EXPECT_THROW(mat1 * mat2, std::invalid_argument);
+    EXPECT_THROW(matrix1 * matrix2, std::invalid_argument);
     
-    TMatrix<> result = mat2 * mat1;
+    TMatrix<> result = matrix2 * matrix1;
     TMatrix<> expected{{2}, {5}};
     EXPECT_EQ(result, expected);
 }
 
 
 TEST(MatrixOperationsTest, AdittionEqualScalarCorrect) {
-    TMatrix<> mat{{1, 2, 3}, {4, 5, 6}};
-    mat += 1;
+    TMatrix<> matrix{{1, 2, 3}, {4, 5, 6}};
+    matrix += 1;
 
     TMatrix<> expected{{2, 3, 4}, {5, 6, 7}};
-    EXPECT_EQ(mat, expected);
+    EXPECT_EQ(matrix, expected);
 }
 
 TEST(MatrixOperationsTest, SubtractionEqualScalarCorrect) {
-    TMatrix<> mat{{1, 2, 3}, {4, 5, 6}};
-    mat -= 1;
+    TMatrix<> matrix{{1, 2, 3}, {4, 5, 6}};
+    matrix -= 1;
 
     TMatrix<> expected{{0, 1, 2}, {3, 4, 5}};
-    EXPECT_EQ(mat, expected);
+    EXPECT_EQ(matrix, expected);
 }
 
 TEST(MatrixOperationsTest, MultiplicationEqualScalarCorrect) {
-    TMatrix<> mat{{1, 2, 3}, {4, 5, 6}};
-    mat *= 2;
+    TMatrix<> matrix{{1, 2, 3}, {4, 5, 6}};
+    matrix *= 2;
 
     TMatrix<> expected{{2, 4, 6}, {8, 10, 12}};
-    EXPECT_EQ(mat, expected);
+    EXPECT_EQ(matrix, expected);
 }
 
 TEST(MatrixOperationsTest, DivisionEqualScalarCorrect) {
-    TMatrix<> mat{{2, 4, 6}, {8, 10, 12}};
-    mat /= 2;
+    TMatrix<> matrix{{2, 4, 6}, {8, 10, 12}};
+    matrix /= 2;
 
     TMatrix<> expected{{1, 2, 3}, {4, 5, 6}};
-    EXPECT_EQ(mat, expected);
+    EXPECT_EQ(matrix, expected);
 }
 
 TEST(MatrixOperationsTest, DivisionEqualScalarIncorrect) {
-    TMatrix<> mat{{1, 2, 3}, {4, 5, 6}};
-    EXPECT_THROW(mat /= 0, std::runtime_error);
+    TMatrix<> matrix{{1, 2, 3}, {4, 5, 6}};
+    EXPECT_THROW(matrix /= 0, std::runtime_error);
 }
 
 TEST(MatrixOperationsTest, AdittionScalarCorrect) {
-    TMatrix<> mat{{1, 2, 3}, {4, 5, 6}};
-    TMatrix<> result = mat + 1;
+    TMatrix<> matrix{{1, 2, 3}, {4, 5, 6}};
+    TMatrix<> result = matrix + 1;
 
     TMatrix<> expected{{2, 3, 4}, {5, 6, 7}};
     EXPECT_EQ(result, expected);
 }
 
 TEST(MatrixOperationsTest, SubtractionScalarCorrect) {
-    TMatrix<> mat{{1, 2, 3}, {4, 5, 6}};
-    TMatrix<> result = mat - 1;
+    TMatrix<> matrix{{1, 2, 3}, {4, 5, 6}};
+    TMatrix<> result = matrix - 1;
 
     TMatrix<> expected{{0, 1, 2}, {3, 4, 5}};
     EXPECT_EQ(result, expected);
 }
 
 TEST(MatrixOperationsTest, MultiplicationScalarCorrect) {
-    TMatrix<> mat{{1, 2, 3}, {4, 5, 6}};
-    TMatrix<> result = mat * 2;
+    TMatrix<> matrix{{1, 2, 3}, {4, 5, 6}};
+    TMatrix<> result = matrix * 2;
 
     TMatrix<> expected{{2, 4, 6}, {8, 10, 12}};
     EXPECT_EQ(result, expected);
 }
 
 TEST(MatrixOperationsTest, DivisionScalarCorrect) {
-    TMatrix<> mat{{2, 4, 6}, {8, 10, 12}};
-    TMatrix<> result = mat / 2;
+    TMatrix<> matrix{{2, 4, 6}, {8, 10, 12}};
+    TMatrix<> result = matrix / 2;
 
     TMatrix<> expected{{1, 2, 3}, {4, 5, 6}};
     EXPECT_EQ(result, expected);
 }
 
 TEST(MatrixOperationsTest, DivisionScalarIncorrect) {
-    TMatrix<> mat{{1, 2, 3}, {4, 5, 6}};
+    TMatrix<> matrix{{1, 2, 3}, {4, 5, 6}};
 
-    EXPECT_THROW(TMatrix<> result = mat / 0, std::runtime_error);
+    EXPECT_THROW(TMatrix<> result = matrix / 0, std::runtime_error);
 }
 
 TEST(MatrixSpecialFunctionsTest, Transpose) {
-    TMatrix<> mat{{1, 2, 3}, {4, 5, 6}};
-    TMatrix<> result = mat.transpose();
+    TMatrix<> matrix{{1, 2, 3}, {4, 5, 6}};
+    TMatrix<> result = matrix.Transpose();
 
     TMatrix<> expected{{1, 4}, {2, 5}, {3, 6}};
     EXPECT_EQ(result, expected);
 }
 
 TEST(MatrixSpecialFunctionsTest, Equality) {
-    TMatrix<> mat1{{1, 2}, {3, 4}};
-    TMatrix<> mat2{{1, 2}, {3, 4}};
-    TMatrix<> mat3{{4, 3}, {2, 1}};
+    TMatrix<> matrix1{{1, 2}, {3, 4}};
+    TMatrix<> matrix2{{1, 2}, {3, 4}};
+    TMatrix<> matrix3{{4, 3}, {2, 1}};
 
-    EXPECT_TRUE(mat1 == mat2);
-    EXPECT_FALSE(mat1 == mat3);
+    EXPECT_TRUE(matrix1 == matrix2);
+    EXPECT_FALSE(matrix1 == matrix3);
 }
 
 TEST(MatrixSpecialFunctionsTest, Printing) {
